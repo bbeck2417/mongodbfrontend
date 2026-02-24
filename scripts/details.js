713e743d11d1fd67421a93e3fdd935dd554834a1
Billy Beck <billys.beck@gmail.com>
2026-02-23 22:45:44 -0500

    Update API endpoint for fetching songs

--- scripts/details.js
+++ scripts/details.js
@@ -3,7 +3,7 @@ addEventListener("DOMContentLoaded", async () => {
   const urlparam = new URLSearchParams(window.location.search);
   const songID = urlparam.get("id");
   console.log(songID);
-  const response = await fetch("http://localhost:3100/api/songs/" + songID);
+  const response = await fetch("https://mongodbbackend-evmy.onrender.com/api/songs/" + songID);
   const song = await response.json();
   console.log(song);
   let heading = "";
